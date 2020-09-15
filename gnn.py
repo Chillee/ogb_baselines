298816ee040a67cd235187113d1589d3094ce24f
Horace He <horacehe2007@yahoo.com>
2020-09-15 14:42:08 -0400

    Update gnn.py

--- gnn.py
+++ gnn.py
@@ -301,7 +301,7 @@ def main():
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--batch_size', type=int, default=64 * 1024)
     parser.add_argument('--lr', type=float, default=0.005)
-    parser.add_argument('--epochs', type=int, default=1000)
+    parser.add_argument('--epochs', type=int, default=500)
     parser.add_argument('--eval_steps', type=int, default=5)
     parser.add_argument('--runs', type=int, default=10)
     args = parser.parse_args()
